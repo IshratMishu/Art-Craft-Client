e0f8ed591ca0cd4caa0a8d5515614e20740f0fcd
IshratMishu <ishratjahan0425@gmail.com>
2024-04-27 07:47:43 +0600

    some css applied

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import CardsData from "../../Pages/CardsData/CardsData";
 import ComingSoon from "../../Sections/ComingSoon";
 import Pottery from "../../Sections/Pottery";
 import Banner from "../Banner/Banner";
+import bg from '../../assets/images/vaseq.png';
 
 const Home = () => {
     const pottery = useLoaderData();
@@ -10,6 +11,10 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Pottery></Pottery>
+            <div className="text-center space-y-3 mt-10 md:mt-5 mb-10" style={{ backgroundImage: `url(${bg})`}}>
+                <p className="text-sm">Latest Products</p>
+                <h1 className="text-2xl">~PERFECT DECOR PIECES FOR DAILY LIFE~</h1>
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 m-4 overflow-hidden" >
                 {
                     pottery.map(potter => <CardsData key={potter._id} potter={potter}></CardsData>)
