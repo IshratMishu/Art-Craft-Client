540fb3364afea92907d3a871d3a834559634a56c
IshratMishu <ishratjahan0425@gmail.com>
2024-04-27 12:01:02 +0600

    table added

--- src/Pages/AllCraftItems/AllCraftItems.jsx
+++ src/Pages/AllCraftItems/AllCraftItems.jsx
@@ -5,12 +5,35 @@ import SingleCraftItem from "../SingleCraftItem/SingleCraftItem";
 const AllCraftItems = () => {
     const allPottery = useLoaderData();
     return (
-         <div className="grid grid-cols-1 md:grid-cols-2 m-3 lg:m-10 gap-10">
-            {
-                allPottery.map(singlePottery => <SingleCraftItem key={singlePottery._id} singlePottery={singlePottery} ></SingleCraftItem>)
-            }
+        <div className="mt-28 ">
+            <div className="overflow-x-auto m-5 md:m-10 bg-base-100 shadow-md shadow-[#81c784] md:p-5 lg:p-10">
+                <table className="table">
+                    <thead>
+                        <tr className="bg-base-100 shadow-md shadow-[#81c784]">
+                            <th></th>
+                            <th>Name</th>
+                            <th>Picture</th>
+                            <th>Sub Category</th>
+                            <th>Price</th>
+                            <th>Details</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {allPottery.map((singlePottery,idx) => (
+                            <SingleCraftItem
+                                key={singlePottery._id}
+                                singlePottery={singlePottery}
+                                idx={idx}
+                            />
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
 
 export default AllCraftItems;
+
+
+
