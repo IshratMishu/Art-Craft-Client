e440cc64f3a5340d49e2ecd44d206026e01ecb0f
IshratMishu <ishratjahan0425@gmail.com>
2024-04-27 13:03:11 +0600

    table for data is added

--- src/Pages/AllCraftItems/AllCraftItems.jsx
+++ src/Pages/AllCraftItems/AllCraftItems.jsx
@@ -1,11 +1,15 @@
 import { useLoaderData } from "react-router-dom";
 import SingleCraftItem from "../SingleCraftItem/SingleCraftItem";
-
+import back from "../../assets/images/vaseq.png";
 
 const AllCraftItems = () => {
     const allPottery = useLoaderData();
     return (
-        <div className="mt-28 ">
+        <div className="mt-32">
+    <div className="text-center space-y-3" style={{ backgroundImage: `url(${back})`}}>
+    <h1 className="text-2xl">~AWESOME COLLECTION FOR YOU!~</h1>
+    <p className="text-sm">Elevate your home decor with our distinctive clay creations, designed to add warmth and character to any space.</p>
+    </div>
             <div className="overflow-x-auto m-5 md:m-10 bg-base-100 shadow-md shadow-[#81c784] md:p-5 lg:p-10">
                 <table className="table">
                     <thead>
