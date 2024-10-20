d9df2d09a9816131a79b3654f3f44c4ccca7e675
IshratMishu <ishratjahan0425@gmail.com>
2024-10-20 19:49:25 +0600

    issues fixed

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
 import { Outlet, ScrollRestoration } from "react-router-dom";
@@ -9,28 +9,30 @@ import { ToastContainer } from 'react-toastify';
 const Root = () => {
     const [showLoading, setShowLoading] = useState(true);
 
-    setTimeout(() => {
-        setShowLoading(false)
-    }, 2000);
+    useEffect(() => {
+        setTimeout(() => {
+            setShowLoading(false)
+        }, 2000);
+    }, [])
 
     return (
         <div>
-            {
-                showLoading ?
-                    (<div className="h-screen flex font-bold text-3xl md:text-5xl items-center justify-center">
-                        ClayPottery  <span className="loading loading-ring loading-lg"></span>
-                        <span className="loading loading-ring loading-md"></span>
-                        <span className="loading loading-ring loading-sm"></span>
-                    </div>)
-                    :
-                    (<div>
-                        <Navbar></Navbar>
-                        <ToastContainer />
-                        <Outlet></Outlet>
-                        <Footer></Footer>
-                        <ScrollRestoration></ScrollRestoration>
-                    </div>)
-            }
+            {showLoading ? (
+                <div className="h-screen flex font-bold text-3xl md:text-5xl items-center justify-center">
+                    ClayPottery
+                    <span className="loading loading-ring loading-lg"></span>
+                    <span className="loading loading-ring loading-md"></span>
+                    <span className="loading loading-ring loading-sm"></span>
+                </div>
+            ) : (
+                <div className="font-poppins">
+                    <Navbar />
+                    <ToastContainer />
+                    <ScrollRestoration />
+                    <Outlet />
+                    <Footer />
+                </div>
+            )}
         </div>
     );
 };
