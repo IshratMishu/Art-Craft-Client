550024f19874f820663430a64e1a8b2365c18f2d
IshratMishu <ishratjahan0425@gmail.com>
2024-10-20 19:53:17 +0600

    some issue fixed

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,5 +1,3 @@
-
-import { useEffect, useState } from "react";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
 import { Outlet, ScrollRestoration } from "react-router-dom";
@@ -7,32 +5,14 @@ import { ToastContainer } from 'react-toastify';
 
 
 const Root = () => {
-    const [showLoading, setShowLoading] = useState(true);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setShowLoading(false)
-        }, 2000);
-    }, [])
 
     return (
-        <div>
-            {showLoading ? (
-                <div className="h-screen flex font-bold text-3xl md:text-5xl items-center justify-center">
-                    ClayPottery
-                    <span className="loading loading-ring loading-lg"></span>
-                    <span className="loading loading-ring loading-md"></span>
-                    <span className="loading loading-ring loading-sm"></span>
-                </div>
-            ) : (
-                <div className="font-poppins">
-                    <Navbar />
-                    <ToastContainer />
-                    <ScrollRestoration />
-                    <Outlet />
-                    <Footer />
-                </div>
-            )}
+        <div className="font-poppins">
+            <Navbar />
+            <ToastContainer />
+            <ScrollRestoration />
+            <Outlet />
+            <Footer />
         </div>
     );
 };
