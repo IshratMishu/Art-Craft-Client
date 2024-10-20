154afd2e9acb56466bb3786795b44208b897d996
IshratMishu <ishratjahan0425@gmail.com>
2024-10-20 19:10:15 +0600

    some issues fixed

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,17 +1,36 @@
 
+import { useState } from "react";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 
+
 const Root = () => {
+    const [showLoading, setShowLoading] = useState(true);
+
+    setTimeout(() => {
+        setShowLoading(false)
+    }, 2000);
+
     return (
         <div>
-            <Navbar></Navbar>
-            <ToastContainer />
-            <Outlet></Outlet>
-            <Footer></Footer>
-            <ScrollRestoration></ScrollRestoration>
+            {
+                showLoading ?
+                    (<div className="h-screen flex font-bold text-3xl md:text-5xl items-center justify-center">
+                        ClayPottery  <span className="loading loading-ring loading-lg"></span>
+                        <span className="loading loading-ring loading-md"></span>
+                        <span className="loading loading-ring loading-sm"></span>
+                    </div>)
+                    :
+                    (<div>
+                        <Navbar></Navbar>
+                        <ToastContainer />
+                        <Outlet></Outlet>
+                        <Footer></Footer>
+                        <ScrollRestoration></ScrollRestoration>
+                    </div>)
+            }
         </div>
     );
 };
